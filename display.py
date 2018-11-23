8024a4ff58208b9c37e1ad688b43b60c1e298d42
FiveRAge <33856475+FiveRAge@users.noreply.github.com>
2018-11-23 17:47:27 +0300

    Add files via upload

---
+++ display.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/robot/xdisplay",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    (rows,cols,channels) = cv_image.shape
+    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    cv_image=cv2.resize(cv_image, (0,0), fx=1, fy=1.06)
+    cv2.imshow("window", cv_image)
+    cv2.waitKey(3)
+
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
